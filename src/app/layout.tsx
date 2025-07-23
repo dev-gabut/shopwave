98fe49fab4a4b46201a834af02cab2127508c092
Blasius Yonas Vikariandi <blasius.yonas@gmail.com>
2025-07-23 06:31:00 +0000

    Change the font to fully sans serif

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
       <body className={cn("font-body antialiased h-full", "font-body")}>
         <AuthProvider>
@@ -41,5 +41,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-    
