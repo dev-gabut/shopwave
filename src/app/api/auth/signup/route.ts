cf199ef2eb61145dbddf04c6319f161548f8b9cc
agungferdi <muhammad_agungf@student.ub.ac.id>
2025-07-29 01:43:16 +0700

    fixing api sign up

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,13 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { signup } from '@/server/controllers/authControllers';
+import { prisma } from '@/lib/prisma'; 
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
   try {
+    await prisma.$connect();
+    
+    const body = await req.json();
     await signup(body);
-    return NextResponse.json({ succuess: true }, { status: 201 });
+    
+    return NextResponse.json({ success: true }, { status: 201 });
   } catch (error: unknown) {
     const errorMessage = error instanceof Error ? error.message : 'Unknown error';
     return NextResponse.json({ error: errorMessage }, { status: 400 });
+  } finally {
+    await prisma.$disconnect();
   }
 }
