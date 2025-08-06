5a6086361734bee9557c0499c56a94a56d8737e7
inovafani <inoveka@gmail.com>
2025-08-06 16:25:37 +0700

    Add photo logic for account

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -15,6 +15,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { useToast } from '@/hooks/use-toast';
 import { Separator } from '@/components/ui/separator';
+import { supabase } from '@/lib/supabase';
 
 export default function AccountPage() {
   const { user, loading } = useAuth();
